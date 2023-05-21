7a0fd42ff437534a522b36b3f635c218dc65c8df
shuang <shuang.lixx@qq.com>
2023-05-21 13:21:09 +0800

    完成了MenuItem的开发，完成NavbarUI的开发

--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -2,14 +2,21 @@
 
 import {AiOutlineMenu} from 'react-icons/ai'
 import Avatar from './Avatar';
+import { useCallback, useState } from 'react';
+import MenuItem from './MenuItem';
 
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value)=>!value)
+  },[])
   return (
     <div className=" relative">
       <div className="flex flex-row items-cen
       gap-3">
         <div
-         onClick={() => {}}
+         onClick={toggleOpen}
          className="
           hidden
           md:block
@@ -53,6 +60,35 @@ const UserMenu = () => {
           </div>
         </div>
       </div>
+
+      {isOpen && (
+        <div className='
+          absolute
+          rounded-xl
+          shadow-md
+          w-[40vw]
+          md:w-3/4
+          bg-white
+          overflow-hidden
+          right-0
+          top-12
+          text-sm
+        '>
+          <div className='flex flex-col cursor-pointer'>
+            <>
+              <MenuItem
+                onClick={() => {}}
+                label='Login'
+              />
+              <MenuItem
+                onClick={() => {}}
+                label='Sign up'
+              />
+            </>
+          </div>
+        </div>
+      )}
+
     </div>
   )
 }
