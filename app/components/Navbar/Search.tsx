989f903085d1fc95e9b6f1fa2dd62da30f57a3c9
shuang <shuang.lixx@qq.com>
2023-05-21 11:30:00 +0800

    完成logo和Search组件的开发

--- app/components/Navbar/Search.tsx
+++ app/components/Navbar/Search.tsx
@@ -1,7 +1,29 @@
+'use client';
+
 const Search = () => {
   return (
-    <div>
-      Search
+    <div
+      className="
+       border-[1px]
+       w-full
+       md:w-auto
+       py-2
+       rounded-full
+       shadow-sm
+       hover:shadow-md
+       transition
+       cursor-pointer
+      "
+    >
+      <div
+      className="flex flex-row items-center justify-between"
+      >
+        <div 
+         className="text-sm font-semibold px-6"
+        >
+          Anywhere
+        </div>
+      </div>
     </div>
   )
 }
