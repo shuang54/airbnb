b5e43215e39b4774aed747951346de4847a4577a
shuang <shuang.lixx@qq.com>
2023-05-26 19:00:46 +0800

    完成上传图片功能

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir:true
+    appDir: true
   },
   images: {
     domains: [
-      "avatars.githubusercontent.com",
-      "lh3.googleusercontent.com"
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+      'res.cloudinary.com'
     ]
   }
 }
